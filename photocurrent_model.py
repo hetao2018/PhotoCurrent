b7ec49e31a087cc6e4b62071f716e3c54a8041ad
hetao2018 <393235966@qq.com>
2020-02-22 10:13:36 +0800

    Nothing

--- photocurrent_model.py
+++ photocurrent_model.py
@@ -31,15 +31,45 @@ def Je(E):
     return J
 
 def Fourier(E):
-    dJ_dt = Ne(E) * E
-    num = 10 * len(t)
-    dJ_dt = np.pad(dJ_dt, (num, num), 'constant')
-    FFt = fft(dJ_dt)
+    # num = 10 * len(t)
+    # dJ_dt = np.pad(dJ_dt, (num, num), 'constant')
+    FFt = fft(E)
     return fftshift(FFt)
 
-plt.xlim(90000,120000)
-co_rotating = co_P(t) + co_S(t)
-counter_rotating = counter_P(t) + counter_S(t)
-plt.plot(abs(Fourier(counter_P(t))))
+co_rotating,counter_rotating = np.zeros(t.size,dtype=complex),np.zeros(t.size,dtype=complex)
+co_rotating.real = co_P(t)
+co_rotating.imag = co_S(t)
+counter_rotating.real = counter_P(t)
+counter_rotating.imag = counter_S(t)
+
+dJ_dt_co_P = Ne(co_rotating) * co_P(t)
+dJ_dt_co_S = Ne(co_rotating) * co_S(t)
+dJ_dt_counter_P = Ne(counter_rotating) * counter_P(t)
+dJ_dt_counter_S = Ne(counter_rotating) * counter_S(t)
+
+
+plt.plot(Fourier(dJ_dt_co_S))
 plt.show()
+
+
+
+'''
+co_theta = np.angle(co_rotating)
+co_abs = abs(co_rotating)
+counter_theta = np.angle(counter_rotating)
+counter_abs = abs(counter_rotating)
+Size = 12
+plt.figure(figsize=(10,5))
+plt.subplot(121,polar=True)
+plt.plot(co_theta,co_abs,linewidth=1.5,color="y",label="Co-rotating")
+plt.rgrids(np.arange(0.01,1.1*max(co_abs),0.03),angle=180)
+plt.legend(loc=(0.7,1),prop={'size': Size})
+plt.subplot(122,polar=True)
+plt.plot(counter_theta,counter_abs,linewidth=1.5,color="c",label="Counter-rotating")
+plt.legend(loc=(0.7,1),prop={'size': Size})
+plt.rgrids(np.arange(0.01,1.1*max(counter_abs),0.03),angle=180)
+plt.tight_layout(pad=1.0, w_pad=4)
+plt.show()
+'''
+
 
